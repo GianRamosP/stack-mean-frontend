4a5831d12a86ba7611bdeb79421326e4d26c1eab
Kale <gian21022001@gmail.com>
2024-05-04 16:39:03 -0500

    add components and pages and shared

---
+++ src/app/shared/heavy-loaders/users-loader.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+Component({
+  selector: 'app-user-loader',
+  standalone: true,
+  imports: [],
+  template: `<h1>Blank</h1>`,
+});
+
+export class UsersLoaderComponent {
+  constructor() {}
+}
---
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { routes } from '../../app.routes';
+
+@Component({
+  selector: 'app-sidemenu',
+  standalone: true,
+  imports: [],
+  templateUrl: './sidemenu.component.html',
+  styleUrl: './sidemenu.component.css',
+})
+export class SidemenuComponent {
+  public menuItems = routes
+    .map((route) => route.children ?? [])
+    .flat()
+    .filter((route) => route && route.path)
+    .filter((route) => !route.path?.includes(':'));
+
+  constructor() {
+    // const dashboardRoutes = routes
+    //   .map((route) => route.children ?? [])
+    //   .flat()
+    //   .filter((route) => route && route.path)
+    //   .filter((route) => !route.path?.includes(':'));
+    // console.log(dashboardRoutes);
+  }
+}
